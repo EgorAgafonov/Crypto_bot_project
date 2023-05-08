44d2a4fbf5726733b55c2f7fad21e9bab1559ce6
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-05-08 14:46:26 +0300

    File_ Unit_23.2_Библиотека Requests_updated

--- Unit_23.2_Библиотека Requests.py
+++ Unit_23.2_Библиотека Requests.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
-r = requests.get(
-    'https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=html')  # делаем запрос на сервер по переданному адресу
-print(r.status_code)
+r = requests.get('https://baconipsum.com/api/?type=meat-and-filler')  # делаем запрос на сервер по переданному адресу
+texts = json.loads(r.content)
+print(type(texts))
+for text in texts:
+    print(text[:50],'\n')
