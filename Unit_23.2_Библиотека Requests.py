3f9f40ae9931e3ac914572a2b2a74af5f1cb83d8
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-05-10 18:31:09 +0300

    File_Unit_23_Telegram's bot practice updated

--- Unit_23.2_Библиотека Requests.py
+++ Unit_23.2_Библиотека Requests.py
@@ -1,7 +1,7 @@
-import requests
-import json
-import lxml.html
-from lxml import etree
+# import requests
+# import json
+# import lxml.html
+# from lxml import etree
                                             # Request запросы:
 
 # r = requests.get('https://baconipsum.com/api/?type=meat-and-filler')
@@ -49,15 +49,32 @@ from lxml import etree
 # print(title)
 
 # 2.
+
+
+# tree = etree.parse('Welcome to Python.org.html', lxml.html.HTMLParser())
+# ul = tree.findall('body/div/div[3]/div/section/div[3]/div[1]/div/ul/li')
+#
+# for li in ul:
+#     a = li.find('a')
+#     print(a.text)
+
+# 3.
 import requests
-import json
 import lxml.html
 from lxml import etree
 
-tree = etree.parse('Welcome to Python.org.html', lxml.html.HTMLParser())
-ul = tree.findall('body/div/div[3]/div/section/div[3]/div[1]/div/ul/li')
+html = requests.get('https://www.python.org/').content  # получим html главной странички официального сайта python
 
+# создадим объект ElementTree. Он возвращается функцией parse()
+tree = etree.parse('Welcome to Python.org.html',
+                   lxml.html.HTMLParser())  # попытаемся спарсить наш файл с помощью html парсера
+
+ul = tree.findall(
+    'body/div/div[3]/div/section/div[3]/div[1]/div/ul/li')  # помещаем в аргумент методу findall скопированный xpath
+
+# создаём цикл в котором мы будем выводить название каждого элемента из списка
 for li in ul:
-    a = li.find('a')
-    print(a.text)
+    a = li.find('a')  # в каждом элементе находим где хранится название. У нас это тег <a>
+    time = li.find('time')
+    print(time.get('datetime'), a.text)  # из этого тега забираем текст - это и будет нашим названием
 
