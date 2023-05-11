d7965a184748e3099e2052dd001900ef763e7b2e
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-05-11 10:49:55 +0300

    File_Unit_23_Redis_tutoriald_added

--- Unit_23_Redis_tutorial.py
+++ Unit_23_Redis_tutorial.py
@@ -1,5 +1,5 @@
 import redis
 
-rds_base = redis.Redis(host='redis-12602.c240.us-east-1-3.ec2.cloud.redislabs.com',
+red = redis.Redis(host='redis-12602.c240.us-east-1-3.ec2.cloud.redislabs.com',
                        port=12602,
                        password='1ovYrkKHIQ8yg9xD2EDUvav6TxedxIoY')
