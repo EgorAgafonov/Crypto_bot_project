1fad9fadf8c8fb771b226682062c4fde528e7d6c
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-05-11 19:50:24 +0300

    'File_app.py_updated'

--- Unit_23.2_Библиотека Requests.py
+++ Unit_23.2_Библиотека Requests.py
@@ -4,19 +4,19 @@ import lxml.html
 from lxml import etree
                                             # Request запросы:
 
-# r = requests.get('https://baconipsum.com/api/?type=meat-and-filler')
-# texts = json.loads(r.content)  # делаем из полученных байтов python объект для удобной работы
-# print(type(texts))  # проверяем тип сконвертированных данных
-#
-# for text in texts:  # выводим полученный текст. Но для того чтобы он влез в консоль оставим только первые 50 символов.
-#     print(text[:50], '\n')
-#
-# r = requests.get('https://api.github.com')
-#
-# d = json.loads(r.content)  # делаем из полученных байтов python объект для удобной работы
-#
-# print(type(d))
-# print(d['following_url']) # обращаемся к полученному объекту как к словарю и попробуем напечатать одно из его значений
+r = requests.get('https://baconipsum.com/api/?type=meat-and-filler')
+texts = json.loads(r.content)  # делаем из полученных байтов python объект для удобной работы
+print(type(texts))  # проверяем тип сконвертированных данных
+
+for text in texts:  # выводим полученный текст. Но для того чтобы он влез в консоль оставим только первые 50 символов.
+    print(text[:50], '\n')
+
+r = requests.get('https://api.github.com')
+
+d = json.loads(r.content)  # делаем из полученных байтов python объект для удобной работы
+
+print(type(d))
+print(d['following_url']) # обращаемся к полученному объекту как к словарю и попробуем напечатать одно из его значений
 
 # Таким образом мы можем удобно превращать данные, полученные из ответа JSON, в объекты структур данных Python
 # с помощью библиотеки JSON, и удобно работать с ними.
@@ -60,16 +60,16 @@ from lxml import etree
 #     print(a.text)
 # 3.
 
-html = requests.get('https://www.python.org/').content  # получим html главной странички официального сайта python
-
-# создадим объект ElementTree. Он возвращается функцией parse()
-tree = etree.parse('Welcome to Python.org.html',
-                   lxml.html.HTMLParser())  # попытаемся спарсить наш файл с помощью html парсера
-
-ul = tree.findall('/body/div/div[3]/div/section/div/div[1]/div/ul/li') # помещаем в аргумент методу findall скопированный xpath
-# создаём цикл в котором мы будем выводить название каждого элемента из списка
-for li in ul:
-    a = li.find('a')  # в каждом элементе находим где хранится название. У нас это тег <a>
-    time = li.find('time')
-    print(time.get('datetime'), a.text)  # из этого тега забираем текст - это и будет нашим названием
+# html = requests.get('https://www.python.org/').content  # получим html главной странички официального сайта python
+#
+# # создадим объект ElementTree. Он возвращается функцией parse()
+# tree = etree.parse('Welcome to Python.org.html',
+#                    lxml.html.HTMLParser())  # попытаемся спарсить наш файл с помощью html парсера
+#
+# ul = tree.findall('/body/div/div[3]/div/section/div/div[1]/div/ul/li') # помещаем в аргумент методу findall скопированный xpath
+# # создаём цикл в котором мы будем выводить название каждого элемента из списка
+# for li in ul:
+#     a = li.find('a')  # в каждом элементе находим где хранится название. У нас это тег <a>
+#     time = li.find('time')
+#     print(time.get('datetime'), a.text)  # из этого тега забираем текст - это и будет нашим названием
 
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import telebot
 import requests
+import json
 
 TOKEN = "6042849022:AAEzmCgXh41PfAbc0F62CcEt9lZicVp2y-c"
 bot = telebot.TeleBot(TOKEN)
@@ -16,16 +17,22 @@ def repeat(message: telebot.types.Message):
 в другую, используя актуальный курс ЦБ РФ на текущую дату.\n' \
            f'Для начала работы введите данные в следующем формате:\n' \
            f' <продаваемая валюта> <покупаемая валюта> <количество продаваемой валюты>.\n' \
-           f'Пример ввода:'
+           f'Пример ввода: \n' \
+           f'Нажмите /values чтобы увидеть список достпных валют.'
     bot.reply_to(message, text)
 
 @bot.message_handler(commands=['values'])
 def repeat(message: telebot.types.Message):
-    text = f'Доступные для конвертации валюты:\n' \
-           f'Российский рубль - RUB\n' \
-           f'Европейский евро - EUR\n' \
-           f'Американский доллар - USD'
+    text = f'Доступные для конвертации валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key))
     bot.reply_to(message, text)
 
+@bot.message_handler(content_types=['text', ])
+def get_price(message: telebot.types.Message):
+    # доллар рубль 1
+    base, quote, amount = message.text.split(' ')
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[base]}&tsyms={keys[quote]}')
+    text = json.loads()
 
 bot.polling(none_stop=True)
