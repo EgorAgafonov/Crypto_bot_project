bb481681cfdd3d78f7abbe19f4b848af354e2398
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-05-13 12:53:27 +0300

    'File_app.py_updated'

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ TOKEN = "6042849022:AAEzmCgXh41PfAbc0F62CcEt9lZicVp2y-c"
 keys = {
     'рубль': 'RUB',
     'евро': 'EUR',
-    'доллар': 'USD'
+    'доллар': 'USD',
+    'юань': 'CNY',
+    'биткоин': 'BTC'
 }
